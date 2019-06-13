7c12ead81c4fff14ade827780295d457d644be9c
Nicholas Sosa <nicholassosa@Nicholass-MacBook-Air.local>
2019-06-13 11:07:54 -0500

    small update to exercise

--- js/external.js
+++ js/external.js
@@ -5,23 +5,26 @@
 // var color = prompt("What is your favorite color?");
 // console.log("Neat, "+color+" is my favorite as well!");
 
+//3.1
 // alert("Thank you for returning  your movie(s).");
 // var rentalDays = Number(prompt("Please enter the total days the movie(s) were rented for: "));
 // var totalPrice = rentalDays*3;
 // alert("For "+rentalDays+" you owe: $"+totalPrice);
 
+//3.2
 // var googleHours = Number(prompt("How  many hours did you work at Google this week?"));
 // var amazonHours = Number(prompt("How many hours did you work at Amazon this week?"));
 // var facebHours = Number(prompt("how many hours did you work at Facebook this week?"));
 // alert("You will be paid $"+(googleHours*400+amazonHours*380+facebHours*350)+" this week.");
 
+//3.3
 // var classTime = Number(prompt("What time does this class start?"));
 // var studentTime = confirm("Click OK if you do NOT have a "+classTime+" class already");
 // var maxClassSize = Number(prompt("What is the max class size?"));
 // var currentClassSize = Number(prompt("What is the current class size?"));
 // var totalClassSize = maxClassSize > currentClassSize;
 // // classTime == studentTime;
-// alert("Student dsoauhga"+totalClassSize && studentTime);
+// alert("Student can enroll in this class."+(totalClassSize && studentTime));
 
 // console.log(classTime);
 // console.log(studentTime);
@@ -29,8 +32,8 @@
 // console.log(currentClassSize);
 // console.log(totalClassSize);
 
-
-var premiumMember = confirm("Are you a premium member of store?");
-var moreThanTwoItems = confirm("And/Or have you bought more than two (2) items?");
-var offerExpirationDate = confirm("Click OK if the offer is NOT expired.");
-alert("The product offer has been accepted."+(premiumMember || moreThanTwoItems) && offerExpirationDate);
+// //3.4
+// var premiumMember = confirm("Are you a premium member of store?");
+// var moreThanTwoItems = confirm("And/Or have you bought more than two (2) items?");
+// var offerExpirationDate = confirm("Click OK if the offer is NOT expired.");
+// alert("The product offer has been accepted."+((premiumMember || moreThanTwoItems) && offerExpirationDate));

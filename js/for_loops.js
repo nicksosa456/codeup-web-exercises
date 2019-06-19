c1d913b0d848d864dba7a32c17584eb1989636ba
Nicholas Sosa <nicholassosa@Nicholass-MacBook-Air.local>
2019-06-19 11:35:18 -0500

    updated for loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,18 +1,27 @@
-function showMultiplicationTable(a) {
-    console.log(a+ " x 1 = "+a*1);
-    console.log(a+ " x 2 = "+a*2);
-    console.log(a+ " x 3 = "+a*3);
-    console.log(a+ " x 4 = "+a*4);
-    console.log(a+ " x 5 = "+a*5);
-    console.log(a+ " x 6 = "+a*6);
-    console.log(a+ " x 7 = "+a*7);
-    console.log(a+ " x 8 = "+a*8);
-    console.log(a+ " x 9 = "+a*9);
-    console.log(a+ " x 10 = "+a*10);
-}
+(function () {
+"use strict";
+
+var  showMutiplicationTable = function(a) {
+    for (var i = 1 ;i <= 10; i++) {
+        console.log(a+" x "+i+" = "+(i*parseInt(a)));
+    }
+};
+    //  Misunderstood directions.
+    // function showMultiplicationTable(a) {
+    //     console.log(a+ " x 1 = "+a*1);
+    //     console.log(a+ " x 2 = "+a*2);
+    //     console.log(a+ " x 3 = "+a*3);
+    //     console.log(a+ " x 4 = "+a*4);
+    //     console.log(a+ " x 5 = "+a*5);
+    //     console.log(a+ " x 6 = "+a*6);
+    //     console.log(a+ " x 7 = "+a*7);
+    //     console.log(a+ " x 8 = "+a*8);
+    //     console.log(a+ " x 9 = "+a*9);
+    //     console.log(a+ " x 10 = "+a*10);
+    // }
 
 function randomNum() {
-    return Math.floor(Math.random() * 200) + 20;
+    return Math.floor(Math.random() * 180) + 20;
 }
 
 
@@ -37,3 +46,4 @@ for (var y = 100; y > 0; y-=5) {
     console.log(y);
 }
 
+})();

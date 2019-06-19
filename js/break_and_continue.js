2ad08d3fa1baf3a3eef3b158d2a06232739ba16b
Nicholas Sosa <nicholassosa@Nicholass-MacBook-Air.local>
2019-06-19 10:45:00 -0500

    finished loops exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,33 +1,55 @@
-function isNumeric(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
+(function () {
 
-// function isItNotNumber(input) {
-//     return
+    var enterNum = parseInt(prompt("Pick an odd number between 1 and 50."));
+
+    function isEven(a) {
+        return a%2===0;
+    }
+
+
+    while (true) {
+        if (isEven(enterNum) || enterNum > 50 || isNaN(enterNum)) {
+            enterNum = parseInt(prompt("Please enter a valid odd number."));
+        } else {
+            console.log("You picked number "+enterNum);
+            break;
+        }
+    }
+
+    for (var num = 1; num < 50; num ++) {
+        if (num%2===0) {
+            continue;
+        } else if (num===enterNum) {
+            console.log("Skipping the number you entered: "+enterNum);
+            continue;
+        }
+        console.log("Here is an odd number: "+num);
+    }
+
+//    TRIED THIS STUFF, DIDN'T WORK OUT VERY WELL
+// function enterNum() {
+//     return parseInt(prompt("Pick an odd number between 1 and 50"));
+//
 // }
-
-// var numberToStop = parseInt(prompt("Pick an odd number between 1 and 50"));
-
-var numberToStop = 0;
-
-while (true) {
-    if (numberToStop === isNumeric() || numberToStop > 50 || numberToStop < 0) {
-
-    }
-}
-
-for (var num = 1; num < 50; num ++) {
-    if (num%2===0) {
-        continue;
-    }
-    console.log("Here is an odd number: "+num);
-}
-
-
-// do {
-//     var numberToStop = parseInt(prompt("Pick an odd number between 1 and 50"));
-//     if(isNaN(numberToStop) === true) {
-//         var numberToStop = parseInt(prompt("Please pick a valid odd number between 1 and 50"));
+// enterNum();
+// var wantNum = enterNum();
+//
+// while (true) {
+//     if (wantNum === false || wantNum > 50 || wantNum === (wantNum%2===0)) {
+//         return parseInt(prompt("Enter a valid odd number."));
+//     } else {
 //         break;
 //     }
-// } while();
+// }
+//
+//
+// for (var num = 1; num < 50; num ++) {
+//     if (num%2===0) {
+//         continue;
+//     } else if (num===wantNum) {
+//         console.log("Skipping the number you entered: "+wantNum);
+//     }
+//     console.log("Here is an odd number: "+num);
+// }
+
+})();

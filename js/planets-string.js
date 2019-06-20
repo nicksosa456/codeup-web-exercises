2365f6ae54f8ce920a8034b38a031ba74118bd04
Nicholas Sosa <nicholassosa@Nicholass-MacBook-Air.local>
2019-06-20 13:55:18 -0500

    small update to string

--- js/planets-string.js
+++ js/planets-string.js
@@ -30,4 +30,17 @@
 
     var listPlanets = "<ul><li>" + planetsArray.join('</li><li>') + "</li></ul>";
 
+    for (var i = 1; i <=100 ; i++) {
+        if (i%3===0 && i%5===0) {
+            console.log('FizzBuzz');
+        } else if (i%5===0) {
+            console.log('Buzz');
+        } else if (i%3===0) {
+            console.log('Fizz');
+        } else {
+            console.log(i);
+        }
+    }
+
+
 })();

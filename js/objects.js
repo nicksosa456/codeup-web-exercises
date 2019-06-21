3b9be8100dbeb431ec55c36234bcd482ec0c6542
Nicholas Sosa <nicholassosa@Nicholass-MacBook-Air.local>
2019-06-21 11:29:24 -0500

    circle exercise

--- js/objects.js
+++ js/objects.js
@@ -173,6 +173,89 @@
 
     var showBookInfo = function(bookObject) {
         createBook("Artemis Fowl", "Eoin Colfer");
-    }
+    };
+
+
+    //BONUS STUFF
+
+    var carArray = [];
+
+    var makeCar = function(make, model) {
+        return {
+            make: make,
+            model: model
+        };
+    };
+
+    carArray.push(makeCar("Mitsubishi", "Pathfinder"));
+
+    console.log(carArray);
+
+    var salesReport =[
+        {
+            title: "Monthly Sales Report",
+            date: "03-17-2015",
+            office: "Codeup",
+            employees: [{
+                employeeNumber: "1",
+                firstName: "Jane",
+                lastName: "Janeway",
+                salesUnits: "3"
+            }, {
+                employeeNumber: "3",
+                firstName: "Tricia",
+                lastName: "Triciason",
+                salesUnits: "5"
+            }, {
+                employeeNumber: "4",
+                firstName: "Jeanette",
+                lastName: "Jeanson",
+                salesUnits: "4"
+            }, {
+                employeeNumber: "5",
+                firstName: "Charles Emerson III",
+                lastName: "Winchester",
+                salesUnits: "2"
+            }, {
+                employeeNumber: "6",
+                firstName: "Chet",
+                lastName: "Chedderson",
+                salesUnits: "8"
+            }, {
+                employeeNumber: "7",
+                firstName: "Chaiam",
+                lastName: "Chaiamson",
+                salesUnits: "12"
+            }, {
+                employeeNumber: "8",
+                firstName: "Dale",
+                lastName: "Dalesinger",
+                salesUnits: "1"
+            }, {
+                employeeNumber: "9",
+                firstName: "Zig",
+                lastName: "Ziglar",
+                salesUnits: "50"
+            }, {
+                employeeNumber: "10",
+                firstName: "Henry",
+                lastName: "Kissinger",
+                salesUnits: "1"
+            }, {
+                employeeNumber: "11",
+                firstName: "Arthur Herbert",
+                lastName: "Fonzarelli",
+                salesUnits: "23"
+            }, {
+                employeeNumber: "12",
+                firstName: "Betty",
+                lastName: "Boop",
+                salesUnits: "67"
+            }
+            ]
+        }
+    ];
+
+
 
 })();

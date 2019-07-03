e3b4490978a9d62c3b3be4440ae2cfea1244d164
Nicholas Sosa <nicholassosa@Nicholass-MacBook-Air.local>
2019-07-03 13:52:59 -0500

    added more cheat codes

--- js/cheat-codes.js
+++ js/cheat-codes.js
@@ -1,6 +1,6 @@
 "use strict";
-$().ready(
-    function(){
+// $().ready(
+//     function(){
 
         $(document).keyup(function(event){
             var key = event.key;
@@ -26,6 +26,36 @@ $().ready(
                 case 'Enter':
                     $('#container').css('background-image', "url(img/PS3_Start.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
                     break;
+                case '1':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_1.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
+                case '2':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_2.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
+                case '3':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_3.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
+                case '4':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_4.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
+                case '5':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_5.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
+                case '6':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_6.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
+                case '7':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_7.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
+                case '8':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_8.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
+                case '9':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_9.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
+                case '0':
+                    $('#container').css('background-image', "url(img/Keyboard_Black_0.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    break;
                 default:
                     $('#container').css('background-image', "url(img/wrong-button.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
                     break;
@@ -34,6 +64,9 @@ $().ready(
 
 
         var konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+        var tmntTwo = ['b', 'a', 'b', 'a', 'ArrowUp', 'ArrowDown', 'b', 'a', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+        var sonic = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'a', 'Enter'];
+        var punch = ['0', '0', '7', '3', '7', '3', '5', '9', '6', '3'];
         var arr = [];
         var count = 0;
 
@@ -43,8 +76,42 @@ $().ready(
                 arr.push(event.key);
                 if (arr.length === konamiCode.length) {
                     $('#container').css('background-image', "url(img/contra.jpg)").css('background-repeat', 'no-repeat').css('background-position', 'center');
-                    var audio = new Audio('audio/contra_title.mp3');
-                    audio.play();
+                    $('#head').text('You added 30 lives!');
+                    var kAudio = new Audio('audio/contra_title.mp3');
+                    kAudio.play();
+                    count = 0;
+                    arr = [];
+                }
+            } else if (event.key === tmntTwo[count]) {
+                count++;
+                arr.push(event.key);
+                if (arr.length === tmntTwo.length) {
+                    $('#container').css('background-image', "url(img/TMNT2-title.png)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    $('#head').text('You added 10 lives!');
+                    var tAudio = new Audio('audio/TMNT2.mp3');
+                    tAudio.play();
+                    count = 0;
+                    arr = [];
+                }
+            } else if (event.key === sonic[count]) {
+                count++;
+                arr.push(event.key);
+                if (arr.length === sonic.length) {
+                    $('#container').css('background-image', "url(img/sonic-title.jpg)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    $('#head').text('Select a Level');
+                    var sAudio = new Audio('audio/sonic-title.mp3');
+                    sAudio.play();
+                    count = 0;
+                    arr = [];
+                }
+            } else if (event.key === punch[count]) {
+                count++;
+                arr.push(event.key);
+                if (arr.length === punch.length) {
+                    $('#container').css('background-image', "url(img/punch-title.gif)").css('background-repeat', 'no-repeat').css('background-position', 'center');
+                    $('#head').text('Fight Mike Tyson');
+                    var pAudio = new Audio('audio/punch-title.mp3');
+                    pAudio.play();
                     count = 0;
                     arr = [];
                 }
@@ -54,4 +121,5 @@ $().ready(
             }
         });
 
-    });
+
+    // });

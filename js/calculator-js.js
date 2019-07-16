8ab58726888823ca28e93d19d1ea157326a37c0c
Nicholas Sosa <nicksosa456@gmail.com>
2019-07-16 15:00:36 -0500

    messing with calc

--- js/calculator-js.js
+++ js/calculator-js.js
@@ -1,26 +1,28 @@
 'use strict';
 
-$('.numbers').click(function(){
-    if ($(this).val()) {
-        let num = $(this).html();
-        $('#leftSide').val(function(i, val){
-            return val + num;
-        })
-    } else if (symbols.html() === true) {
-        let num = $(this).html();
-        $('#rightSide').val(function(i, val){
-            return val + num;
-        })
-    }
-});
 
-const symbols = $('.symbol').click(function(){
+const symbolFunc = $('.symbol').click(function(){
     let num = $(this).html();
     $('#middle').val(function(){
         return num;
     })
 });
 
-$('.clear').click(function (){
+const numberFunc = $('.numbers').click(function(){
+    if ($('.symbol').html()===true) {
+        let num = $(this).html();
+        $('#rightSide').val(function(i, val){
+            return val + num;
+        })
+    } else {
+        let num = $(this).html();
+        $('#leftSide').val(function(i, val){
+            return val + num;
+        })
+    }
+});
+
+
+const clearFunc = $('.clear').click(function (){
     $('.text-area').val('');
 });
